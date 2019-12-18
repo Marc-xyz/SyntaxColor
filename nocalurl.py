fe9b06f1298fab63b6a95f4f420f27aecc207835
Marc-xyz <fermat.bohr@gmail.com>
2019-12-18 23:46:57 +0100

    add more 1k links

--- nocalurl.py
+++ nocalurl.py
@@ -1,7 +1,6 @@
 import wikipedia as wiki
 h="https://en.wikipedia.org/wiki/"
-file=open("url1000.txt","w")
-for j in range(7,10):
+for j in range(11,21):
     file=open("url1000_"+str(j)+".dat","w")
     for num in range(1000):
         nam=wiki.random(1)
